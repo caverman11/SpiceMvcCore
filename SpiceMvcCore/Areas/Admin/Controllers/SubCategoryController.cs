6a53d2cd5090071ac9e44a337b47e72072b0cb17
David Caver <caverman11@hotmail.com>
2019-08-11 22:02:25 -0500

    Added SubCategory View and Controller. Started on an Extension method.

---
+++ SpiceMvcCore/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpiceMvcCore.Data;
+using SpiceMvcCore.Models.ViewModels;
+
+namespace SpiceMvcCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var subCategories = await _db.SubCategory.Include(s => s.Category).ToListAsync();
+            return View(subCategories);
+        }
+
+
+        public async Task<IActionResult> Create()
+        {
+            //Create the ViewModel and populate it from DB which has been dependency injected.
+            var model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                SubCategoryList = await _db.SubCategory.OrderBy(n => n.Name).Select(p => p.Name).Distinct().ToListAsync()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ SpiceMvcCore/Models/SubCategory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpiceMvcCore.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "SubCategoryh Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name ="Category")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
